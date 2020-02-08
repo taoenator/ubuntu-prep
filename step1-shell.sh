4a032b9528fd32f8da10cc4ec180cddd66c00b25
taoenator <taoenator@users.noreply.github.com>
2020-02-08 13:15:48 -0800

    Fix Powerline font installation

--- step1-shell.sh
+++ step1-shell.sh
@@ -24,6 +24,7 @@ sudo apt -y install powerline fonts-powerline
 cd
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts
 mv PowerlineSymbols.otf ~/.fonts/
 mkdir -p .config/fontconfig/conf.d #if directory doesn't exists
 
